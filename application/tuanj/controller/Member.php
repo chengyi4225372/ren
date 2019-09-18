a2a65dd008c79520b86ba967df525f8bf42a3f58
moonl <347755658>
2019-09-18 17:25:21 +0800

    add

---
+++ application/tuanj/controller/Member.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace app\tuanj\controller;
+
+use think\Db;
+use controller\BasicAdmin;
+use service\DataService;
+
+class Member extends BasicAdmin {
+
+    private $dataform = 'r_member';
+
+    public function index() {
+        $this->title = '用户列表';
+        list($get, $db) = [$this->request->get(), Db::name($this->dataform)->where('status',1)];
+        (isset($get['keywords']) && $get['keywords'] !== '') && $db->whereLike('users', "%{$get['keywords']}%");
+        if (isset($get['date']) && $get['date'] !== '') {
+            list($start, $end) = explode(' - ', $get['date']);
+            $db->whereBetween('create_at', ["{$start} 00:00:00", "{$end} 23:59:59"]);
+        }
+        return parent::_list($db->order('id desc'));
+    }
+
+
+    /**
+     * 添加
+     * @return type
+     */
+    public function add() {
+        return $this->_form($this->dataform, 'form');
+    }
+
+    /**
+     * 编辑
+     * @return type
+     */
+    public function edit() {
+        return $this->_form($this->dataform, 'form');
+    }
+
+    /**
+     * 添加成功回跳处理
+     * @param bool $result
+     */
+    protected function _form_result($result) {
+        if ($result !== false) {
+            list($base, $spm, $url) = [url('@admin'), $this->request->get('spm'), url('tuanj/member/index')];
+            $this->success('数据保存成功！', "{$base}#{$url}?spm={$spm}");
+        }
+    }
+
+    /**
+     * 删除
+     * @throws \think\Exception
+     * @throws \think\exception\PDOException
+     */
+    public function del() {
+        if (DataService::update($this->dataform)) {
+            $this->success("删除成功!", '');
+        }
+        $this->error("删除失败, 请稍候再试!");
+    }
+
+    /**
+     * 禁用
+     * @throws \think\Exception
+     * @throws \think\exception\PDOException
+     */
+    public function forbid() {
+        if (DataService::update($this->dataform)) {
+            $this->success("禁用成功!", '');
+        }
+        $this->error("禁用失败, 请稍候再试!");
+    }
+
+    /**
+     * 禁用
+     * @throws \think\Exception
+     * @throws \think\exception\PDOException
+     */
+    public function resume() {
+        if (DataService::update($this->dataform)) {
+            $this->success("启用成功!", '');
+        }
+        $this->error("启用失败, 请稍候再试!");
+    }
+
+}
